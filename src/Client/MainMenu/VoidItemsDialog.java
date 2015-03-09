01fe11ed819c6025e3a7e3032d55566a945f96ef
[Michael] <[michael.goldsmith@student.manchester.ac.uk]>
2015-03-09 19:16:53 +0000

    write add to stock method!!! and then test

--- src/Client/MainMenu/VoidItemsDialog.java
+++ src/Client/MainMenu/VoidItemsDialog.java
@@ -19,6 +19,8 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
@@ -30,8 +32,9 @@ public class VoidItemsDialog extends javax.swing.JDialog {
     private final Tab oldTab;
     private final Tab newTab;  
     private final JButton submitButton = new JButton("Submit");
-    ArrayList<Pair<Pair<JCheckBox, Item>, Pair<Component, Component>>> cBoxesOldTab = new ArrayList<>();
-    ArrayList<Pair<Pair<JCheckBox, Item>, Pair<Component, Component>>> cBoxesNewTab = new ArrayList<>();
+    private final JButton cancelButton = new JButton("Cancel");    
+    ArrayList<Pair<Pair<JCheckBox, Item>, Pair<Component, Component>>> cBoxesOldTab;
+    ArrayList<Pair<Pair<JCheckBox, Item>, Pair<Component, Component>>> cBoxesNewTab;
     
     /**
      * Creates new form VoidItemsDialog
@@ -43,7 +46,8 @@ public class VoidItemsDialog extends javax.swing.JDialog {
         super(parent, modal);
         this.oldTab = tab.getFirst();
         this.newTab = tab.getSecond();
-        addCheckBoxes();
+        cBoxesOldTab = addCheckBoxes(oldTab);
+        cBoxesNewTab = addCheckBoxes(newTab);
         initComponents();
     }
 
@@ -56,11 +60,17 @@ public class VoidItemsDialog extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        titlePanel = new javax.swing.JPanel();
         mainPanelScrollPane = new javax.swing.JScrollPane();
         mainPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
+        getContentPane().add(titlePanel);
+        titlePanel.setLayout(new GridLayout(0,3));
+        titlePanel.add(new JLabel("Item"));
+        titlePanel.add(new JLabel("Wastage"));
+        titlePanel.add(new JLabel("Amount"));
 
         mainPanel.setLayout(new javax.swing.BoxLayout(mainPanel, javax.swing.BoxLayout.LINE_AXIS));
         mainPanelScrollPane.setViewportView(mainPanel);
@@ -83,56 +93,103 @@ public class VoidItemsDialog extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
    
-    private void addCheckBoxes()
+    private ArrayList<Pair<Pair<JCheckBox, Item>, Pair<Component, Component>>> 
+        addCheckBoxes(Tab tab)
     {
-        for (Item item : oldTab.getItems())    
+        ArrayList<Pair<Pair<JCheckBox, Item>, Pair<Component, Component>>> 
+            returnList = new ArrayList<>();
+        for (Item item : tab.getItems())    
         {
-            JCheckBox jcb = new JCheckBox(item.getQuantity() + " " + item.getName());
-            Component first;
+            final Component first;
+            final Component second;
+
             if (item.stockCount)
+            {
                 first = new JCheckBox("Wasted");
+                first.setEnabled(false);
+            } // if
             else
-                first = new JLabel("");
+                first = new JLabel("Stock Count Off");
             
-            Component second;
+            
             if (item.getQuantity() > 1)
             {
                 second = new JTextField("");
-            }
-            else second = new JLabel("");
+                second.setEnabled(false);
+            } // if
+            else second = new JLabel("Only 1 Item");
             
-            cBoxesOldTab.add(new Pair(new Pair(jcb, item), new Pair(first, second)));   
+            final JCheckBox jcb = new JCheckBox(item.getQuantity() + " " + item.getName());
+            jcb.addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e) 
+                {
+                    if (jcb.isSelected())
+                    {
+                        first.setEnabled(true);
+                        second.setEnabled(true);
+                    } // if
+                    else
+                    {
+                        first.setEnabled(false);
+                        second.setEnabled(false);                        
+                    } // else
+                } // actionPerformed               
+            });
+            
+            returnList.add(new Pair(new Pair(jcb, item), new Pair(first, second)));   
         } // for
         
-        for (Item item : newTab.getItems())        
-        {
-            JCheckBox jcb = new JCheckBox(item.getQuantity() + " " + item.getName());
-            Component first;
-            if (item.stockCount)
-                first = new JCheckBox("Wasted");
-            else
-                first = new JLabel("");
-            
-            Component second;
-            if (item.getQuantity() > 1)
-            {
-                second = new JTextField("");
-            }
-            else second = new JLabel("");
-            
-            cBoxesNewTab.add(new Pair(new Pair(jcb, item), new Pair(first, second))); 
-        } // for
+        return returnList;
     } 
     
     public Pair<Tab, Tab> startDialog()
     {
+        JPanel exitPanel = new JPanel();
+        exitPanel.setLayout(new GridLayout(1,0));
         final VoidItemsDialog thisInstance = this;
-       submitButton.addActionListener(new ActionListener()
+        submitButton.addActionListener(new ActionListener()
         {
-            public void removeItems(ArrayList<Pair<Pair<JCheckBox, Item>, Pair<Component, Component>>> cBoxes, Tab tab)
+            public void addToStock(Item item, int quantity)
+            {
+                
+            }
+            
+            public boolean removeItems(ArrayList<Pair<Pair<JCheckBox, Item>, 
+                Pair<Component, Component>>> cBoxes, Tab tab)
+                    
+                   
             {
                 CopyOnWriteArrayList<Item> temp = new  CopyOnWriteArrayList<>();  
                 temp.addAll(tab.getItems());
+ 
+                // the first for check for errors
+                for (Pair<Pair<JCheckBox, Item>, Pair<Component, Component>> jCB : cBoxes)
+                {
+                    if (jCB.getFirst().getFirst().isSelected() && (jCB.getSecond().getSecond() instanceof JTextField))
+                    {
+                        int amount = 0;
+                        try
+                        {
+                            JTextField tf = (JTextField)jCB.getSecond().getSecond();
+                            amount = Integer.parseInt(tf.getText());
+                        }
+                        catch (NumberFormatException e)
+                        {
+                            JOptionPane.showMessageDialog(thisInstance, "Incorrect number format! Please check your formatting!");
+                            return false;
+                        }
+
+                        if (amount >= jCB.getFirst().getSecond().getQuantity() ||
+                                amount <= 0)
+                        {
+                            JOptionPane.showMessageDialog(thisInstance, "Incorrect number format! Please check your formatting!");
+                            return false;
+                        }
+                    } // if
+                }  // for               
+                
                 
                 for (Pair<Pair<JCheckBox, Item>, Pair<Component, Component>> jCB : cBoxes)
                 {
@@ -140,24 +197,57 @@ public class VoidItemsDialog extends javax.swing.JDialog {
                     {
                         for (Item i : temp)
                             if (jCB.getFirst().getSecond() == i)
-                                temp.remove(i);
-                    }
+                            {
+                                if (jCB.getFirst().getSecond().getQuantity() == 1)
+                                {
+                                    temp.remove(i); 
+                                    if (i.stockCount)
+                                        addToStock(i, 1);
+                                } // if
+                                else
+                                {
+                                    JTextField tf = (JTextField)jCB.getSecond().getSecond();
+                                    int amount = Integer.parseInt(tf.getText());     
+                                    if (amount == jCB.getFirst().getSecond().getQuantity())
+                                       temp.remove(i);
+                                    else
+                                        i.setQuantity(i.getQuantity() - amount);
+                         
+                                    if (i.stockCount)
+                                        addToStock(i, 1);
+                                } // else
+                            } // if                  
+                    } // if
                 } // for     
                 
                 tab.removeAll();
                 for (Item i :temp )
                     tab.addItem(i);
-                
+                return true;
             }
             @Override
             public void actionPerformed(ActionEvent e) 
             {
-                removeItems(cBoxesOldTab, oldTab);
-                removeItems(cBoxesNewTab, newTab); 
+                if (removeItems(cBoxesOldTab, oldTab)
+              &&  removeItems(cBoxesNewTab, newTab)) 
                 thisInstance.setVisible(false);
             } // actionPerformed
         });
-        getContentPane().add(submitButton);
+       
+        cancelButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                thisInstance.dispose();
+            }
+            
+        });
+        getContentPane().add(exitPanel);
+        exitPanel.add(submitButton);
+        exitPanel.add(cancelButton);
+        
         pack();
         
         this.setVisible(true);
@@ -193,6 +283,7 @@ public class VoidItemsDialog extends javax.swing.JDialog {
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 VoidItemsDialog dialog = new VoidItemsDialog(new JDialog(), true, null);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
@@ -209,5 +300,6 @@ public class VoidItemsDialog extends javax.swing.JDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel mainPanel;
     private javax.swing.JScrollPane mainPanelScrollPane;
+    private javax.swing.JPanel titlePanel;
     // End of variables declaration//GEN-END:variables
 }
