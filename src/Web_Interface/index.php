69e7bca58e5d1d9ec618cb50445a2d0ac5c54d15
[Michael] <[michael.goldsmith@student.manchester.ac.uk]>
2015-03-10 17:41:47 +0000

    Using bootstrap to make GUI Web Interface

--- src/Web_Interface/index.php
+++ src/Web_Interface/index.php
@@ -3,7 +3,7 @@
     <head>
 <?php echo includeBootStrap(); ?>
     </head>
-    <body onload="alert(<?php echo __FILE__; ?>)">
+    <body onload="alert('<?php echo __FILE__; ?>')">
 <?php echo displayNavBar(); ?>
     </body>
 </html>
