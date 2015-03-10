3ae44784c125316764be2e3034090c5d5c8f24ae
[Michael] <[michael.goldsmith@student.manchester.ac.uk]>
2015-03-10 17:19:22 +0000

    Using bootstrap to make GUI Web Interface

--- src/Web_Interface/addItem.php
+++ src/Web_Interface/addItem.php
@@ -159,7 +159,7 @@ and open the template in the editor.
         <title>Insert Item</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php includeBootStrap(); ?>
+        <?php echo includeBootStrap(); ?>
     </head>
     <body>
         
