286742ded3c194fddd12f254c5321b83288f8de9
goldy1992 <goldy13_1992@hotmail.co.uk>
2015-10-24 14:25:20 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/Server/Server.java
    	src/main/java/Server/ServerRunThread.java
    	src/main/java/Server/StartServer.java
    	target/RestaurantManagementSystem-1.0-SNAPSHOT.jar
    	target/classes/Server/Server.class
    	target/classes/Server/StartServer.class

---
+++ server/src/main/java/com/mike/server/DemoMain.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mike.server;
+
+import javax.jms.Queue;
+ 
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+ 
+public class DemoMain {
+ 
+  public static void main(String[] args) {
+    // init spring context
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
+         
+    // get bean from context
+    JmsMessageSender jmsMessageSender = (JmsMessageSender)ctx.getBean("jmsMessageSender");
+         
+    // send to default destination 
+    jmsMessageSender.send("hello JMS");
+         
+    // send to a code specified destination
+    Queue queue = new ActiveMQQueue("AnotherDest");
+    jmsMessageSender.send(queue, "hello Another Message");
+   
+    // close spring application context
+    ((ClassPathXmlApplicationContext)ctx).close();
+  }
+ 
+}
